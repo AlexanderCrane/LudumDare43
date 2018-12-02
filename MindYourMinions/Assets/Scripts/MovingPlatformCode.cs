ea99f60255013022a7c912c538e17d41b8243a2b
Katie <katie.lucas.a@ku.edu>
2018-12-02 17:08:14 -0600

    Edits to Platform and Target

---
+++ MindYourMinions/Assets/Scripts/MovingPlatformCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformCode : MonoBehaviour
+{
+    public float delta = .5f;  // Amount to move up and down from the start point
+    public float speed = .5f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
