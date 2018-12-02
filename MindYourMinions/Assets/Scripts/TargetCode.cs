86a732705ab2d9fbe66ad58f0bc7c8893302da2c
Katie <katie.lucas.a@ku.edu>
2018-12-02 16:44:07 -0600

    Target

---
+++ MindYourMinions/Assets/Scripts/TargetCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCode : MonoBehaviour
+{
+
+    public float delta = 10f;  // Amount to move left and right from the start point
+    public float speed = 7.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
