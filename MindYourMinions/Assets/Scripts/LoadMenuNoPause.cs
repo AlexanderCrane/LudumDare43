063a7479c07e679a68832cc911b4ae8daa1dc47a
AlexanderCrane <InnovateTheWorld@yahoo.com>
2018-12-02 20:41:52 -0600

    FINAL COMMIT YEEHAW

---
+++ MindYourMinions/Assets/Scripts/LoadMenuNoPause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenuNoPause : MonoBehaviour {
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Trying to press button");
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
--- MindYourMinions/Assets/Scripts/Loader.cs
+++ MindYourMinions/Assets/Scripts/Loader.cs
@@ -26,11 +26,25 @@ public class Loader : MonoBehaviour {
         SceneManager.LoadScene(0);
     }
 
-    public void LoadLevel1()
+    public void LoadInstructions()
     {
         Time.timeScale = 1;
         Debug.Log("Trying to press button");
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadCredits()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Trying to press button");
+        SceneManager.LoadScene(4);
+    }
+
+    public void LoadLevel1()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Trying to press button");
+        SceneManager.LoadScene(2);
     }
 
     public void ExitGame()
