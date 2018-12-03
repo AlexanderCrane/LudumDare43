ece50d2cfe76d148a29018c37c6bd2d8544f5552
AlexanderCrane <InnovateTheWorld@yahoo.com>
2018-12-02 18:40:28 -0600

    uh

---
+++ MindYourMinions/Assets/Scripts/Loader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour {
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Time.timeScale = 1;
+    }
+}
